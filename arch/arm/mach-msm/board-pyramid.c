e4d3c845d03eb618a239ce2fc9a37170f78dca87
Amphorion <amphorion1@gmail.com>
2014-03-04 00:20:24 +0100

    set ondemand 2 phase freq to 810Mhz

--- arch/arm/mach-msm/board-pyramid.c
+++ arch/arm/mach-msm/board-pyramid.c
@@ -6289,10 +6289,10 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
-	set_two_phase_freq(1134000);
+	set_two_phase_freq(810000);
 #endif
 #ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
-	id_set_two_phase_freq(1134000);
+	id_set_two_phase_freq(810000);
 #endif
 
 #ifdef CONFIG_CPU_FREQ_GOV_BADASS_2_PHASE
